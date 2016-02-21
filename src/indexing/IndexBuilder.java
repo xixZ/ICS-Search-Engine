13bdfa35891a105eaad4d9fc2e7b7d29c5abf052
haoruiw <henry91w@gmail.com>
2016-02-20 18:00:04 -0700

    haorui_new

--- src/indexing/IndexBuilder.java
+++ src/indexing/IndexBuilder.java
@@ -1,24 +1,50 @@
 package indexing;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
+
 public class IndexBuilder {
+	
     HashMap<String, Posting> revertedIndex = new HashMap<>();
-    //public void buildIndex(ArrayList<String> tokens, Integer docID){
-    	
-    //}
+    
+    public void buildIndex(String[] tokens, Integer docID,Integer wordCount){
+    	int cnt = 1;
+		for(String i : tokens){
+			if(!revertedIndex.containsKey(i)){//new word for big table
+				Posting post = new Posting();
+				post.wordFreq=1;
+				Map <Integer, ArrayList<Integer>> docAndPosition = new HashMap<>();
+				ArrayList<Integer> newList = new ArrayList<>(500);
+				newList.add(wordCount+cnt);
+				docAndPosition.put(docID, newList);
+				post.posting=docAndPosition;
+				revertedIndex.put(i, post);
+			}else{//old word for big table
+				revertedIndex.get(i).wordFreq++;
+				if(revertedIndex.get(i).posting.containsKey(docID)){//the word has the page record
+					revertedIndex.get(i).posting.get(docID).add(wordCount+cnt);
+				}else{//the word does not have the page record
+					ArrayList<Integer> newList = new ArrayList<>(500);
+    				newList.add(wordCount+cnt);
+    				revertedIndex.get(i).posting.put(docID, newList);
+				}
+			}
+			cnt++;
+		}
+    }
     
     
-    public List<String> tokenizeFile(String fileText){
-		StringTokenizer st = new StringTokenizer(fileText,"\t\r\n ,,.;:'\""+"!?@#$%^&*_-=+`~/\\"+"()[]{}<>");  
-	    List<String> tokensList = new ArrayList<String>();
-		while (st.hasMoreTokens()) {  
-			tokensList.add(st.nextToken().toLowerCase());
-			//System.out.println(st.nextToken()); 
-	    }
-		return tokensList;
+    public String[] tokenizeFile(String fileText){
+    	String[]tokens=(fileText.toLowerCase()).split("[^a-zA-Z0-9]");		
+		return tokens;
 	}
+    
+
 }
--- src/indexing/Main.java
+++ src/indexing/Main.java
@@ -2,9 +2,14 @@ package indexing;
 
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -22,57 +27,29 @@ import java.util.Set;
 
 
 public class Main {
-	
-	
-    public static void main(String[] args) throws FileNotFoundException, IOException{
-    	
-    	HashMap <String,Posting> indexTable = new HashMap<>();
-        
+    public static void main(String[] args) throws FileNotFoundException, IOException{        
         IndexBuilder iB = new IndexBuilder();
-        
-    	int pageCount = 0;
+
+    	int docID = 0;
 		int wordCount = 0;
-		
 		//===================================READ FILE
 		try (BufferedReader br = new BufferedReader(new FileReader("./file/myfile1.txt"))) {
 		    for(String line; (line = br.readLine()) != null; ) {
 		        if(line.equals("##--------------------------------------------------------##")){
 		        	//System.out.println("Page #"+cnt+": "+wordsCnt);
-		        	pageCount++;
+		        	docID++;
 		        	wordCount=0;
 		        }else{
-		    		List <String> tokens= iB.tokenizeFile(line);
-		    		int cnt = 1;
-		    		for(String i : tokens){
-		    			if(!indexTable.containsKey(i)){//not contain
-		    				Posting post = new Posting();
-		    				post.wordFreq=1;
-		    				Map <Integer, ArrayList<Integer>> docAndPosition = new HashMap<>();
-		    				docAndPosition.put(pageCount, new ArrayList<Integer>(wordCount+cnt));
-		    				post.posting=docAndPosition;
-		    				indexTable.put(i, post);
-		    			}else{
-		    				indexTable.get(i).wordFreq++;
-		    				
-		    				if(indexTable.get(i).posting.containsKey(pageCount)){
-		    					indexTable.get(i).posting.get(pageCount).add(wordCount+cnt);
-		    				}else{
-		    					indexTable.get(i).posting.put(pageCount, new ArrayList<Integer>(wordCount+cnt));
-		    				}
-		    			}
-		    			cnt++;
-		    		}
-		    		wordCount+=tokens.size();
+		    		String[] tokens= iB.tokenizeFile(line);
+		    		
+		    		iB.buildIndex(tokens, docID, wordCount);
+		    		
+		    		wordCount+=tokens.length;
 		        }
 		    }
 		}
 		
 		Scanner user_input = new Scanner(System.in);
-		
-		
-		//for(String i:indexTable.keySet()){
-			//System.out.println(i);
-		//}
 		
 		//user input:
 		String query1;
@@ -82,7 +59,7 @@ public class Main {
 		
 		
 		//fetch result  HUGE CHANGE NEED TO BE HAPPEN HERE TOOOOOO SLOWWWW:
-		Set<Integer> resultPageNumber = indexTable.get(query1).posting.keySet();
+		Set<Integer> resultPageNumber = iB.revertedIndex.get(query1).posting.keySet();
 		List<String> returnURL = new ArrayList<>();
 		
 		System.out.println("Bro, here is your results: ");
@@ -100,4 +77,30 @@ public class Main {
 		}
 		
     }
+    public void storeMap(HashMap<String, Posting> map, Integer tableNumber){
+	    //write to file 
+	    try{
+	    File file=new File("./file/tables/table"+tableNumber.toString());
+	    FileOutputStream fos=new FileOutputStream(file);
+	        ObjectOutputStream oos=new ObjectOutputStream(fos);
+	        oos.writeObject(map);
+	        oos.flush();
+	        oos.close();
+	        fos.close();
+	    }catch(Exception e){}
+	}
+	public static HashMap<String, Posting> PRINTmap(String[] args) throws IOException{
+		HashMap<String,Posting> mapInFile = new HashMap<String,Posting>();
+   		try{
+            File toRead=new File("D:\\test\\hashmap\\mapOfmyfile558");
+            FileInputStream fis=new FileInputStream(toRead);
+            ObjectInputStream ois=new ObjectInputStream(fis);
+            mapInFile.putAll((HashMap<String,Posting>)ois.readObject());
+            ois.close();
+            fis.close();
+
+        }catch(Exception e){}
+   		return mapInFile;
+	}
+		
 }
