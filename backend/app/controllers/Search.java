d5c030ee8193583ab95a6b04aef656dc071c8f32
XiZ93 <xiz13@uci.edu>
2016-02-23 00:07:21 -0800

    UI ready

--- backend/app/controllers/Search.java
+++ backend/app/controllers/Search.java
@@ -6,9 +6,17 @@ import play.mvc.*;
 import views.html.*;
 
 public class Search extends Controller {
-
+    final Integer numOfResults = 10;
     public Result search(String query) {
-        return ok("received query: " + query);
+        String[] title = new String[numOfResults];
+        String[] url = new String[numOfResults];
+        String[] content = new String[numOfResults];
+        for(int i = 0; i < numOfResults; i ++) {
+            title[i] = Integer.toString(i);
+            url[i] = "http://google.com";
+            content[i] = "I am the " + Integer.toString(i) + "th result";
+        }
+        return ok(index.render(title, url, content, numOfResults));
     }
 
 }
